d48bcde586ff58d89e8473f00476ad555b0862ea
Maor Magori <maor@magori.online>
2022-06-22 18:34:32 +0300

    style: ♻️ Cleaned up dirty code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { query } = require("express");
 const express = require("express"),
-    config = require("./config"),
-    cors = require("cors"),
-    { getSubs } = require("./wizdom"),
-    landing = require("./landingTemplate");
+  config = require("./config"),
+  cors = require("cors"),
+  getSubs = require("./wizdom"),
+  landing = require("./landingTemplate");
 
 const addon = express();
 addon.use(cors());
@@ -12,16 +11,16 @@ addon.use(cors());
  * The addon manifest: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
  */
 const manifest = {
-    id: "xyz.stremio.wizdom",
-    contactEmail: "maor@magori.online",
-    version: process.env.npm_package_version,
-    catalogs: [],
-    resources: ["subtitles"],
-    types: ["movie", "series"],
-    name: "Wizdom Subtitles",
-    description:
-        "An unofficial Stremio addon for Hebrew subtitles from wizdom.xyz. Developed by Maor Development",
-    logo: "https://i.ibb.co/KLYK0TH/wizdon256.png",
+  id: "xyz.stremio.wizdom",
+  contactEmail: "maor@magori.online",
+  version: process.env.npm_package_version,
+  catalogs: [],
+  resources: ["subtitles"],
+  types: ["movie", "series"],
+  name: "Wizdom Subtitles",
+  description:
+    "An unofficial Stremio addon for Hebrew subtitles from wizdom.xyz. Developed by Maor Development",
+  logo: "https://i.ibb.co/KLYK0TH/wizdon256.png",
 };
 
 /**
@@ -30,39 +29,39 @@ const manifest = {
  * @param {*} data 		The data to respond with.
  */
 const respond = function (res, data) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Content-Type", "application/json");
-    res.send(data);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Content-Type", "application/json");
+  res.send(data);
 };
 
 //manifest request.
 addon.get("/manifest.json", function (req, res) {
-    respond(res, manifest);
+  respond(res, manifest);
 });
 
 //Landing page request.
 addon.get("/", function (req, res) {
-    res.set("Content-Type", "text/html");
-    res.send(landing(manifest));
+  res.set("Content-Type", "text/html");
+  res.send(landing(manifest));
 });
 
 //Addon's readme request
 addon.get("/readme.md", (req, res) => {
-    res.sendFile(`${__dirname}/README.md`);
+  res.sendFile(`${__dirname}/README.md`);
 });
 
 //Subtitles request.
 addon.get("/subtitles/:type/:imdbId/:query.json", async (req, res) => {
-    try {
-        var filename = req.params.query.split("=").pop();
+  try {
+    var filename = req.params.query.split("=").pop();
 
-        const subtitles = await getSubs(req.params.imdbId, filename);
-        respond(res, { subtitles: subtitles });
-    } catch (err) {
-        console.log("get subs error:");
-        console.log(err);
-    }
+    const subtitles = await getSubs(req.params.imdbId, filename);
+    respond(res, { subtitles: subtitles });
+  } catch (err) {
+    console.log("get subs error:");
+    console.log(err);
+  }
 });
 
 /**
@@ -86,6 +85,6 @@ addon.get("/subtitles/:type/:imdbId/:query.json", async (req, res) => {
 
 //Starting the addon
 addon.listen(config.port, function () {
-    console.log(config);
-    console.log(`Add-on Repository URL: ${config.local}/manifest.json`);
+  console.log(config);
+  console.log(`Add-on Repository URL: ${config.local}/manifest.json`);
 });
