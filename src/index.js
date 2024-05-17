93a23c2b912892fd7ee31cf9ea2b013319ad9bd9
Nitzan <nitzantomer1998@gmail.com>
2024-05-17 08:04:03 +0300

    Update Index Final Form

--- src/index.js
+++ src/index.js
@@ -1,90 +1,9 @@
-const express = require("express"),
-  baseConfig = require("./configs/baseConfig"),
-  manifestConfig = require("./configs/manifestConfig"),
-  config = require("./config"),
-  cors = require("cors"),
-  { getSubs, downloadSubZip } = require("./wizdom"),
-  landing = require("./landingTemplate"),
-  unzipper = require("unzipper"),
-  morganBody = require("morgan-body"),
-  logger = require("./logger"),
-  requestId = require("express-request-id");
+import express from "express";
+
+import baseConfig from "./configs/baseConfig.js";
+import indexRoute from "./routes/indexRoute.js";
+
 
 const addon = express();
-addon.use(cors());
-addon.use(requestId({ setHeader: false }));
-
-// Setting up the logger
-morganBody(addon, {
-  prettify: false,
-  timezone: "Israel",
-  logRequestBody: false,
-  stream: config.remoteLogging ? logger : null,
-  noColors: true,
-  includeNewLine: false,
-  logIP: true,
-  logRequestId: true,
-  skip: (req, res) => {
-    return !req.path.includes("subtitles");
-  },
-});
-
-/**
- * Adds simple headers to a response.
- * @param {import("superagent").Response} res 	The request's response object
- * @param {*} data 		The data to respond with.
- */
-const respondWithHeaders = function (res, data) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Content-Type", "application/json");
-  res.send(data);
-};
-
-//manifest request.
-addon.get("/manifest.json", function (req, res) {
-  respondWithHeaders(res, manifest);
-});
-
-//Landing page request.
-addon.get("/", function (req, res) {
-  res.set("Content-Type", "text/html");
-  res.send(landing(manifest));
-});
-
-//Addon's readme request
-addon.get("/README.md", (req, res) => {
-  res.sendFile(`${__dirname}/README.md`);
-});
-
-//Subtitles request.
-addon.get("/subtitles/:type/:imdbId/:query?.json", async (req, res) => {
-  try {
-    let filename;
-    if (req.params.query) filename = req.params.query.split("=").pop();
-
-    const subtitles = await getSubs(req.params.imdbId, filename);
-    respondWithHeaders(res, { subtitles: subtitles });
-  } catch (err) {
-    console.error("get subs error: ", err);
-  }
-});
-
-/**
- * unzips Wizdom zip files and send the srt file in it.
- */
-addon.get("/srt/:id.srt", async (req, res) => {
-  try {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.setHeader("Content-Type", "application/octet-stream; charset=utf-8");
-
-    const srtContent = await downloadSubZip(req.params.id);
-
-    res.send(srtContent)
-  } catch (err) {
-    console.error("error occurred while sending unzipped srt file: ", err);
-  }
-});
-
+addon.use("/", indexRoute);
 addon.listen(baseConfig.PORT, () => { console.log(`Add-on Repository URL: ${baseConfig.BASE_URL}/manifest.json`); });
