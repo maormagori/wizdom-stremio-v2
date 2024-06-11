aaad8953b81a731e32c90e24061007755afc3a3b
Maor Magori <maor@magori.online>
2024-06-12 01:36:49 +0300

    lint

--- test/e2e/data/manifest.mock.js
+++ test/e2e/data/manifest.mock.js
@@ -1,18 +1,14 @@
-const version = require("../../../package.json").version;
+const version = require('../../../package.json').version;
 
 module.exports = {
-  "id": "xyz.stremio.wizdom",
-  "contactEmail": "maor@magori.online",
-  "catalogs": [],
-  "resources": [
-    "subtitles",
-  ],
-  "types": [
-    "movie",
-    "series",
-  ],
-  "name": "Wizdom Subtitles",
-  "description": "An unofficial Stremio addon for Hebrew subtitles from wizdom.xyz. Developed by Maor Development",
-  "logo": "https://i.ibb.co/KLYK0TH/wizdon256.png",
-  "version": version,
+  id: 'xyz.stremio.wizdom',
+  contactEmail: 'maor@magori.online',
+  catalogs: [],
+  resources: ['subtitles'],
+  types: ['movie', 'series'],
+  name: 'Wizdom Subtitles',
+  description:
+    'An unofficial Stremio addon for Hebrew subtitles from wizdom.xyz. Developed by Maor Development',
+  logo: 'https://i.ibb.co/KLYK0TH/wizdon256.png',
+  version: version,
 };
