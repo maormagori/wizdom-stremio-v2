95f558d8e38cf6177f4b04ec19ad73e898ffb839
Maor Magori <maor@magori.online>
2024-06-30 16:02:58 +0300

    Changed e2e to match new api endpoint

--- test/e2e/specs/subtitles.spec.js
+++ test/e2e/specs/subtitles.spec.js
@@ -7,9 +7,11 @@ const MOCK_TITLE_ID = `tt1234567`;
 const MOCK_SUB_ID1 = 66054;
 const MOCK_SUB_ID2 = 66055;
 
-const mockTitle = (titleId, response, status = 200) => {
+const mockTitle = (titleId, season, episode, response, status = 200) => {
   nock(`https://wizdom.xyz/`)
-    .get(`/api/releases/${titleId}`)
+    .get(
+      `/api/search?action=by_id&imdb=${titleId}&season=${season}&episode=${episode}`
+    )
     .reply(status, response);
 };
 
@@ -33,14 +35,15 @@ describe('GET /subtitles/:type/:imdbId/:query?.json', () => {
   after(() => {
     nock.cleanAll();
   });
+  beforeEach(() => {
+    nock.cleanAll();
+  });
   it('should return movie subtitle', async () => {
-    mockTitle(MOCK_TITLE_ID, {
-      subs: [
-        {
-          id: MOCK_SUB_ID1,
-        },
-      ],
-    });
+    mockTitle(MOCK_TITLE_ID, undefined, undefined, [
+      {
+        id: MOCK_SUB_ID1,
+      },
+    ]);
     const expectedSubs = createSubtitleArray([MOCK_SUB_ID1]);
     const testUrl = `/subtitles/movie/${MOCK_TITLE_ID}/extraArgs.json`;
 
@@ -50,17 +53,11 @@ describe('GET /subtitles/:type/:imdbId/:query?.json', () => {
   it('should return episode subtitle', async () => {
     const mockSeason = 2;
     const mockEpisode = 3;
-    mockTitle(MOCK_TITLE_ID, {
-      subs: {
-        [mockSeason]: {
-          [mockEpisode]: [
-            {
-              id: MOCK_SUB_ID1,
-            },
-          ],
-        },
+    mockTitle(MOCK_TITLE_ID, mockSeason, mockEpisode, [
+      {
+        id: MOCK_SUB_ID1,
       },
-    });
+    ]);
     const expectedSubs = createSubtitleArray([MOCK_SUB_ID1]);
     const testUrl = `/subtitles/series/${MOCK_TITLE_ID}:${mockSeason}:${mockEpisode}/extraArgs.json`;
 
@@ -68,18 +65,16 @@ describe('GET /subtitles/:type/:imdbId/:query?.json', () => {
   });
 
   it('should return sorted subtitles', async () => {
-    mockTitle(MOCK_TITLE_ID, {
-      subs: [
-        {
-          id: MOCK_SUB_ID1,
-          version: 'sub1',
-        },
-        {
-          id: MOCK_SUB_ID2,
-          version: 'sub2',
-        },
-      ],
-    });
+    mockTitle(MOCK_TITLE_ID, undefined, undefined, [
+      {
+        id: MOCK_SUB_ID1,
+        versioname: 'sub1',
+      },
+      {
+        id: MOCK_SUB_ID2,
+        versioname: 'sub2',
+      },
+    ]);
     const expectedSubs = createSubtitleArray([MOCK_SUB_ID2, MOCK_SUB_ID1]);
     const testUrl = `/subtitles/movie/${MOCK_TITLE_ID}/filename=sub2.json`;
 
@@ -89,6 +84,8 @@ describe('GET /subtitles/:type/:imdbId/:query?.json', () => {
   it('should return empty subtitles for missing title', async () => {
     mockTitle(
       MOCK_TITLE_ID,
+      undefined,
+      undefined,
       {
         statusCode: 500,
         message: 'Internal Server Error',
@@ -101,22 +98,18 @@ describe('GET /subtitles/:type/:imdbId/:query?.json', () => {
   });
 
   it('should return empty subtitles for missing episode', async () => {
-    mockTitle(MOCK_TITLE_ID, {
-      subs: [],
-    });
+    mockTitle(MOCK_TITLE_ID, undefined, undefined, undefined);
     const testUrl = `/subtitles/series/${MOCK_TITLE_ID}:3:/extraArgs.json`;
 
     await makeRequestAndAssertResponse(testUrl, []);
   });
 
   it('should return title subtitles when no extra args were given', async () => {
-    mockTitle(MOCK_TITLE_ID, {
-      subs: [
-        {
-          id: MOCK_SUB_ID1,
-        },
-      ],
-    });
+    mockTitle(MOCK_TITLE_ID, undefined, undefined, [
+      {
+        id: MOCK_SUB_ID1,
+      },
+    ]);
     const expectedSubs = createSubtitleArray([MOCK_SUB_ID1]);
     const testUrl = `/subtitles/movie/${MOCK_TITLE_ID}.json`;
 
